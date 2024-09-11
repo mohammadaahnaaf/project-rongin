0b72f6f51b9c498f9bba4817266b5192f2d53e1c
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2024-09-11 22:19:48 +0600

    style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,11 @@ export default function Home() {
       <Head>
         <title>রঙিন | Rongin</title>
       </Head>
-      <main className={`grid grid-rows-[20px_1fr_20px] bg-gradient-to-tr from-pink-600 via-purple-800 to-rose-700 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}>
-        <div className="flex text-3xl flex-col gap-8 row-start-2 items-center sm:items-start">
-
-          Colorful
+      <main className={`grid p-10 bg-gradient-to-tr from-pink-600 via-purple-800 to-rose-700 items-center justify-items-center min-h-screen`}>
+        <div className="flex text-3xl">
+          <div className='rounded-2xl ring-4 w-[300px] grid items-center justify-center h-[500px] mx-auto ring-white'>
+            <p>Colorful</p>
+          </div>
         </div>
 
       </main>
